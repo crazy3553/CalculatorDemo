11a9d3206cd4376ffa03b8e5949120d69d98d915
Nitesh Kumar <nitesh.kumar@intertecsys.com>
2020-03-04 18:53:57 +0530

    added polyfills

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Calculator } from "../Component/Calculator";
+import "babel-polyfill";
 export class App extends Component {
   render() {
     return (
